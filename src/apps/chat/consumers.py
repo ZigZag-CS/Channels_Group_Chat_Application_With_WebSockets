628dfc62e9fb3c012fd79b56cc86ec9d7f60f6ee
zigzag <ps96068@yahoo.com>
2021-05-19 22:41:37 +0300

    02-04 - not work huinea-huinea

--- src/apps/chat/consumers.py
+++ src/apps/chat/consumers.py
@@ -8,13 +8,15 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
 
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_' + str(self.room_name)
+        self.room_group_name = 'chat_'+str(self.room_name)
         async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
+
 
     def disconnect(self):
 
         async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+
 
     def recieve(self, text_data):
 
